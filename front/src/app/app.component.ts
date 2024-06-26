290bb43bcfe4a47e8100c1e861a274691dd0c9f6
Moisesgp1 <moises_g_125@hotmail.com>
2024-06-26 18:59:19 -0500

    avance front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarraNavegacionComponent } from './component/barra-navegacion/barra-navegacion.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    BarraNavegacionComponent,
+    FooterComponent,
+  ],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BarraNavegacionComponent } from './component/barra-navegacion/barra-navegacion.component';
+import { InicioComponent } from './component/inicio/inicio.component';
+import { FooterComponent } from './component/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent},
+
+
+];
