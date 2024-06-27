1857b31137fb044267fa9e07d864af4def0e1a07
Moisesgp1 <moises_g_125@hotmail.com>
2024-06-27 18:02:15 -0500

    front update

---
+++ front/src/app/component/guarderia/guarderia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guarderia',
+  standalone: true,
+  imports: [],
+  templateUrl: './guarderia.component.html',
+  styleUrl: './guarderia.component.css'
+})
+export class GuarderiaComponent {
+
+}
---
+++ front/src/app/component/perros/perros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perros',
+  standalone: true,
+  imports: [],
+  templateUrl: './perros.component.html',
+  styleUrl: './perros.component.css'
+})
+export class PerrosComponent {
+
+}
