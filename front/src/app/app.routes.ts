501cacc2eb26b9fc0a90b4efd6412e7e99d31029
Moisesgp1 <moises_g_125@hotmail.com>
2024-06-28 19:08:16 -0500

    respaldo moises antes del js calendar

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { BarraNavegacionComponent } from './component/barra-navegacion/barra-navegacion.component';
 import { InicioComponent } from './component/inicio/inicio.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { ServiciosComponent } from './component/servicios/servicios.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent},
-
+{path: 'servicios', component: ServiciosComponent}
 
 ];
