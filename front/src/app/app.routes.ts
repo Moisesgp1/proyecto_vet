0c3fa786d7592c1fc9e46faf3840cdc840e6ea0e
Luis Plazas <luisdplazas@gmail.com>
2024-07-02 18:31:20 -0500

    actualizacion luis

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { InicioComponent } from './component/inicio/inicio.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { AccesoriosComponent } from './component/accesorios/accesorios.component';
 import { ServiciosComponent } from './component/servicios/servicios.component';
+import { CompraComponent } from './component/compra/compra.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'accesorios', component: AccesoriosComponent },
   { path: '', component: InicioComponent },
-  { path: 'servicios/:nombre', component: ServiciosComponent }
+  { path: 'servicios/:nombre', component: ServiciosComponent },
+  { path:'compra', component: CompraComponent }
 ]
 
 
